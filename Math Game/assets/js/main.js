fe4ee21e6fe8d9b95e0394644ae1db3ddbe1ff6d
syler11 <nemeth.szilard82@gmail.com>
2021-12-11 21:35:46 +0000

    button eventListener added

--- Math Game/assets/js/main.js
+++ Math Game/assets/js/main.js
@@ -1,4 +1,23 @@
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGame() {
+    let num1 = Math.floor(Math.random()* 25) + 1;
+    let num2 = Math.floor(Math.random()* 25) + 1;
 
 }
 
