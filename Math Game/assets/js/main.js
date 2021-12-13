4c3e00515ef0c4493f8fad494d1c09dd94358c6e
syler11 <nemeth.szilard82@gmail.com>
2021-12-13 10:42:19 +0000

    calculateCorrectAnswer function is ready

--- Math Game/assets/js/main.js
+++ Math Game/assets/js/main.js
@@ -6,27 +6,48 @@ document.addEventListener("DOMContentLoaded", function(){
     for (let button of buttons) {
         button.addEventListener("click", function(){
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+runGame("addition");
+
 })
 
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random()* 25) + 1;
     let num2 = Math.floor(Math.random()* 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type:" ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+
+}
 
 }
 
 function checkAnswer() {
-
+    
 }
 
 function calculateCorrectAnswer() {
-
+    let operand1 = parseInt(document.getElementById(`operand1`).innerText);
+    let operand2 = parseInt(document.getElementById(`operand2`).innerText);
+    let operator = document.getElementById(`operator`).innerText;
+    
+    if (operator === "+") {
+        return[operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented oprator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
+    
 }
 
 function incremetScore() {
@@ -37,7 +58,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById(`operand1`).textContent = operand1;
+    document.getElementById(`operand2`).textContent = operand2;
+    document.getElementById(`operator`).textContent = "+";
     
 }
 
@@ -52,4 +76,3 @@ function displayMultiplyQuestion() {
 function displayDivideQuestion() {
 
 }
-
