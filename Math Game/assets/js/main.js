68b8194f2975a1f34bfa71126b822df1435e16e9
syler11 <nemeth.szilard82@gmail.com>
2021-12-14 15:48:01 +0000

    favicon added

--- Math Game/assets/js/main.js
+++ Math Game/assets/js/main.js
@@ -28,7 +28,9 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "substract") {
         displaySubstractQuestion(num1, num2);
-    }else {
+    } else if (gameType === "divide") {
+        displayDivideQuestion(num1, num2);
+    } else {
         alert(`Unknown game type:" ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
 
@@ -65,7 +67,9 @@ function calculateCorrectAnswer() {
         return[operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return[operand1 - operand2, "substract"];
-    }else {
+    } else if (operator === "/") {
+        return[operand1 / operand2, "divide"];
+    } else {
         alert(`Unimplemented oprator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -109,5 +113,8 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivideQuestion() {
+    document.getElementById(`operand1`).textContent = (operand1 * operand2);
+    document.getElementById(`operand2`).textContent = operand2;
+    document.getElementById(`operator`).textContent = "/";
 
 }
